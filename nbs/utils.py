2a6166788fac3b4a4449f112fd43bc97eb09a2e5
Seyed Naser Razavi <razavinaser@gmail.com>
2021-09-15 23:34:13 +0100

    Adding confusion matrix computation and plotting

--- nbs/utils.py
+++ nbs/utils.py
@@ -1,13 +1,11 @@
-def generate_bigrams(x):
-    ''' Genterate all unigrams and bigrams for the input text.
-    
-        Inputs:
-            -x: a text (str)
-    '''     
-    n_grams = set(zip(*[x[i:] for i in range(2)]))
-    for n_gram in n_grams:
-        x.append(' '.join(n_gram))
-    return x
+import torch
+import tqdm
+import pandas as pd
+import seaborn as sn
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import confusion_matrix
+from IPython.display import clear_output
 
 
 def display_classification_result(sentence, label, label_idx):
@@ -19,3 +17,41 @@ def display_classification_result(sentence, label, label_idx):
             - label_idx: index of the predicted label in the label vocab.
     '''
     print(f"{label:8s}\x1b[{41 + 2 * label_idx}m" + sentence + "\x1b[m")
+    
+    
+def compute_confusion_matrix(model, iterator):
+    """ Compute confusion matrix.
+
+        Inputs:
+            - model: a pretrained model
+            - iterator: a torchtext iterator that iterates over a torchtext dataset
+
+        Outputs:
+            - Confusion matrix
+    """    
+    model.eval()
+    
+    true_labels, pred_labels = [], []
+    
+    with torch.no_grad():
+        for batch in tqdm.tqdm(iterator):
+            prediction = model(batch.text)
+            true_labels += [label.item() for label in batch.label]
+            pred_labels += [label.item() for label in prediction.argmax(1)]
+    
+    clear_output(True)
+    return confusion_matrix(true_labels, pred_labels, normalize='true'), true_labels, pred_labels
+
+
+def plot_confusion_matrix(cm, labels):
+    """ Plot the confusion matrix
+
+        Inputs:
+            - cm: a confusion matrix (numpy array)
+            - labels: class labels to be shown on the vertical and horizontal axes of confusion matrix
+    """
+    df_cm = pd.DataFrame(cm*100, index=labels, columns=labels)
+    plt.figure(figsize = (10,7))
+    plt.title('Confusion Matrix')
+    sn.heatmap(df_cm, annot=True, cmap=plt.cm.Blues)
+    plt.show()   
